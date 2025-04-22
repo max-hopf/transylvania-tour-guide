b275d826882c9b0589a7d0d63b63f2b7eda103ba
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-04-22 22:09:35 +0300

    Optimize images for deployment with vite-plugin-imagemin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/transylvania-tour-guide/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      pngquant: {
+        quality: [0.7, 0.9],
+        speed: 3,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    })
+  ],
 })
