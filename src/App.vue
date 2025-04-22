3e30d381f45672b0924179375cfd43cb6cf1844e
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-04-22 21:55:47 +0300

    Update router settings

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
-import HeaderNavBar from './components/HeaderNavBar.vue';
-import HeroSection from './components/HeroSection.vue';
-import FooterSection from './components/FooterSection.vue';
-import AboutUsSection from './components/AboutUsSection.vue';
-import OurActivitiesSection from './components/OurActivitiesSection.vue';
-import TestimonialsSection from './components/TestimonialsSection.vue';
-import BlogSection from './components/BlogSection.vue';
+// import HeaderNavBar from './components/HeaderNavBar.vue';
+// import HeroSection from './components/HeroSection.vue';
+// import FooterSection from './components/FooterSection.vue';
+// import AboutUsSection from './components/AboutUsSection.vue';
+// import OurActivitiesSection from './components/OurActivitiesSection.vue';
+// import TestimonialsSection from './components/TestimonialsSection.vue';
+// import BlogSection from './components/BlogSection.vue';
+// import HomePage from './components/HomePage.vue';
 </script>
 
 <template>
   <router-view />
-  <div>Hello from App.vue!</div>
 </template>
-
 
 <style scoped>
 /* Add global styles or layout styles here if needed */
