93518c1e62f9ad83249440850b6f0f5aa0c2ae11
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-05-12 13:55:12 +0300

    Optimize web performance by improve logo img loading

--- src/components/logoImages.js
+++ src/components/logoImages.js
@@ -1,10 +1,15 @@
-import ttg_logo_1_400w_jpeg from '../assets/ttg-logo-1.png';
+import ttg_logo_400w_png from '../assets/ttg-logo-400w.png';
+import ttg_logo_800w_png from '../assets/ttg-logo-800w.png';
+import ttg_logo_1600w_png from '../assets/ttg-logo-1600w.png';
+import ttg_logo_400w_webp from '../assets/ttg-logo-400w.webp';
+import ttg_logo_800w_webp from '../assets/ttg-logo-800w.webp';
+import ttg_logo_1600w_webp from '../assets/ttg-logo-1600w.webp';
 
 export const images = [
   {
-    jpeg: [ttg_logo_1_400w_jpeg],
-    webp: [],
-    fallback: ttg_logo_1_400w_jpeg,
+    jpeg: [ttg_logo_400w_png, ttg_logo_800w_png, ttg_logo_1600w_png],
+    webp: [ttg_logo_400w_webp, ttg_logo_800w_webp, ttg_logo_1600w_webp],
+    fallback: ttg_logo_400w_png,
     alt: 'Transylvania Tour Guide Logo'
   }
 ];
