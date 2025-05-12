4e986e12400cf4b7fe8e733e35e229119e748bf3
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-05-12 22:33:14 +0300

    Refactor animations css and make animations smoother

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import './assets/parallax-bg.css';
 import './style.css';
+import './animations.css';
 import App from './App.vue';
 import router from './router';
 
