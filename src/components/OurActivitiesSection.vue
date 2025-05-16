5715ac7d3dd206b0ed56e1e469b62cbc04983720
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-05-16 19:41:08 +0300

    Fix inconsistend activity card size

--- src/components/OurActivitiesSection.vue
+++ src/components/OurActivitiesSection.vue
@@ -300,8 +300,8 @@ onBeforeUnmount(() => {
 }
 
 .activity-img {
-  width: 100%;
-  height: 100%;
+  width: 350px;
+  height: 210px;
   object-fit: cover;
   border-radius: 1rem;
   box-shadow: 0 2px 10px rgba(0,0,0,0.06);
