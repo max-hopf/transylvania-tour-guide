e05d9d62f1aedfe39d98a66875758c10f8e3805c
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-06-17 13:40:09 +0300

    Delete general website section animation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import './assets/parallax-bg.css';
 import './style.css';
-import './animations.css';
 import App from './App.vue';
 import router from './router';
 
