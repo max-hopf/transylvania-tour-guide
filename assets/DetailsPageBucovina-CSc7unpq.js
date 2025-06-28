197b1c19da98697f7af8c4e0e58ef500bd16e2cf
max-hopf <max-hopf@users.noreply.github.com>
2025-06-28 20:28:17 +0000

    deploy: 568364209bf1decd30a889db6398ae62cbf2a58c

---
+++ assets/DetailsPageBucovina-CSc7unpq.js
@@ -0,0 +1,25 @@
+import{_ as s,c as r,e as a,d as t,u as o,s as l,F as c,b as u}from"./index-rEh_-zQ1.js";import{D as d,a as g,T as p,b as m}from"./TabbedInterface-bK_-lhIi.js";const y="/transylvania-tour-guide/assets/bucovina-title-img-Ben4JWfC.jpg",f="/transylvania-tour-guide/assets/1660902684259_l-CfrY54Cp.jpg",h="/transylvania-tour-guide/assets/20230428_174131_p-B-Jwbnzh.jpg",v="/transylvania-tour-guide/assets/20230430_171243_l-uAW_jVxg.jpg",b="/transylvania-tour-guide/assets/20230501_100744_l-D41DOb3q.jpg",_="/transylvania-tour-guide/assets/20230627_102455_l-XoDudbnZ.jpg",w="/transylvania-tour-guide/assets/20240404_174445_p-CyMw_T9p.jpg",j="/transylvania-tour-guide/assets/20240405_111741_p-C8t8a75v.jpg",D="/transylvania-tour-guide/assets/20240405_111837_p-DfYpEWc3.jpg",B="/transylvania-tour-guide/assets/20240727_190617_p-_wKssdWB.jpg",C="/transylvania-tour-guide/assets/20240914_162445_l-DnB9NaEG.jpg",k="/transylvania-tour-guide/assets/20240915_170504_p-D6MH1DUH.jpg",T="/transylvania-tour-guide/assets/20241015_100725_p-DST8wgPC.jpg",x={__name:"DetailsPageBucovina",setup(E){const i=[{src:f,alt:"",orientation:"landscape"},{src:h,alt:"",orientation:"portrait"},{src:v,alt:"",orientation:"landscape"},{src:b,alt:"",orientation:"landscape"},{src:_,alt:"",orientation:"landscape"},{src:w,alt:"",orientation:"portrait"},{src:j,alt:"",orientation:"portrait"},{src:D,alt:"",orientation:"portrait"},{src:B,alt:"",orientation:"portrait"},{src:C,alt:"",orientation:"landscape"},{src:k,alt:"",orientation:"portrait"},{src:T,alt:"",orientation:"portrait"}],n=[{label:"What’s Included",content:`<ul>
+      <li>Professional guidance</li>
+      <li>2 nights’ accommodation in family-owned traditional guesthouses (all en-suite)</li>
+      <li>Half-board meals featuring organic, locally sourced ingredients</li>
+      <li>All entrance fees and transportation costs</li>
+    </ul>
+    `},{label:"Highlights of the Tour",content:`
+     <strong>Explore key attractions such as:</strong>
+      <ul>
+        <li>UNESCO-Painted Monasteries, including Voroneț</li>
+        <li>Black pottery in Marginea</li>
+        <li>Vama Egg Museum</li>
+      </ul><br>
+      <strong>Experience Romanian Culture through Slow Travel:</strong><br />
+      Immerse yourself in authentic countryside living with stunning landscapes, fresh air, and warm hospitality. Delight in home-grown produce and traditional Romanian dishes.
+      If possible, plan your visit to include a Sunday, when locals don their finest folk costumes and attend church.
+      `},{label:"Flexibility and Extensions",content:`
+    The itinerary is adaptable based on your preferences, weather, and local events. You may extend your trip by one night to deepen your slow travel experience for an additional €195 per person—a rare opportunity to unwind in today’s fast-paced world.
+    <br /><br />
+    <strong>Additional Information</strong><br />
+    The itinerary may be modified on-site depending on weather or local events.
+    <br /><br />
+    <strong>Departure Options and Booking Details</strong><br />
+    While the tour typically begins in Cluj-Napoca, it can also start from different Transylvanian cities, which is easily accessible by train, though Cluj-Napoca is preferred.
+    `}];return(I,e)=>(u(),r(c,null,[a(d,{title:"Bukovina Tour from Cluj-Napoca",description:"",backgroundImage:o(y)},null,8,["backgroundImage"]),a(g,{duration:"3 days",price:"from 590 € / person",groupSize:"2 – 7 participants"}),e[0]||(e[0]=t("section",{class:"activity-details-section overview-section"},[t("div",{class:"activity-details-container"},[t("p",{class:"activity-details-intro"}," Bukovina is a picturesque region in northeastern Romania, renowned for its stunning painted monasteries, rich cultural heritage, and beautiful natural landscapes. Often called the “Land of Painted Churches,” Bucovina’s UNESCO World Heritage sites feature vibrant frescoes that depict religious and historical scenes. The region offers a unique blend of history, tradition, and unspoiled nature, making it a captivating destination for travelers seeking authentic Romanian culture and serene countryside. ")])],-1)),a(p,{tabs:n}),a(m,{images:i}),a(l)],64))}},N=s(x,[["__scopeId","data-v-4f1704de"]]);export{N as default};
